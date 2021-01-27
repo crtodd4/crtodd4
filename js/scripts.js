cd510ce2cdcf0eef5a03e881e25b08c13c60f6e5
Cameron Todd <crtodd4@gmail.com>
2021-01-27 14:12:40 -0800

    setup random generator -setup random background colors -refactored to use array.length

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 console.log("OK");
+
+const backgrounds = [
+  'bg0'
+  'bg1'
+  'bg2'
+];
+
+var numRan;
+numRan = Math.floor(Math.random() * backgrounds.length);
+
+let pageBackground = document.querySelector('body');
+
+pageBackground.classname = backgrounds[numRand];
