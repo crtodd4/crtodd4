2da68dc482b5040474e6f4e5653344f7779c70aa
Cameron Todd <crtodd4@gmail.com>
2021-03-07 14:51:10 -0800

    Added lightGallery

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,10 @@
 console.log("OK");
 
-let backgrounds = [
-  'bg0'
-  'bg1'
-  'bg2'
-];
+const backgrounds = ["bg0", "bg1", "bg2"];
 
-var numRan;
-numRan = Math.floor(Math.random() * backgrounds.length);
+var numRand;
+numRand = Math.floor(Math.random() * backgrounds.length);
 
-let pageBackground = document.querySelector('body');
+let pageBackground = document.querySelector("body");
 
-pageBackground.classname = backgrounds[numRand];
+pageBackground.className = backgrounds[numRand];
